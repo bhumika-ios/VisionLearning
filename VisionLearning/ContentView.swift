de1a9d9b025a5919a4c0580d5382322fd0c7d737
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-02-01 13:30:21 +0530

    add button then append list

--- VisionLearning/ContentView.swift
+++ VisionLearning/ContentView.swift
@@ -11,13 +11,27 @@ import RealityKitContent
 
 struct ContentView: View {
     @State private var todoLists: [TodoList] = []
+    @State private var selectedTodoList: TodoList? = nil
+    
     var body: some View {
         NavigationSplitView {
             List(todoLists){ list in
-                
+                Button(list.title){
+                    selectedTodoList = list
+                }
+            }
+            .navigationTitle("TodoList")
+            .toolbar{
+                Button(action: {
+                    let list = TodoList(title: "List\(todoLists.count)", items: [])
+                    todoLists.append(list )
+                }, label: {
+                    Image(systemName: "plus")
+                })
+               
             }
         } detail: {
-            <#code#>
+            
         }
 
     }
