89924ba4c3a4ae11cdc735c00cbba3c81b979e29
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-02-02 17:17:22 +0530

    seprate todolistview and add task in detailview

---
+++ VisionLearning/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  VisionLearning
+//
+//  Created by Bhumika Patel on 02/02/24.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
