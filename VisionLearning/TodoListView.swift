de21333812917f49463f02cf3d559a6d664db820
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-02-14 15:21:32 +0530

    delete and edit perform with swiftdata

--- VisionLearning/TodoListView.swift
+++ VisionLearning/TodoListView.swift
@@ -13,35 +13,20 @@ struct TodoListView: View {
     @State var list: TodoList
     @State private var showAddTodoAlert: Bool = false
     @State private var newTodoTitle: String = ""
+    
     var body: some View {
         List {
-            Section ("Todo") {
-                ForEach(list.items.filter { $0.isDone == false }) { item in
-                    HStack {
-                        Button {
-                            item.isDone.toggle()
-                        } label: {
-                            Image (systemName: item.isDone ? "circle.fill" : "circle")
-                        }
-                            Text(item.title)
-                        Spacer ()
-                    }
-                }
-            }
-            Section("Done"){
-                ForEach(list.items.filter { $0.isDone }) { item in
-                    HStack {
-                        Button {
-                            item.isDone.toggle()
-                        } label: {
-                            Image (systemName: item.isDone ? "circle.fill" : "circle")
-                        }
-                        Text(item.title)
-                        Spacer()
-                    }
-                }
-            }
-        }
+                   Section("Todo") {
+                       ForEach(list.items.filter { !$0.isDone }) { item in
+                           TodoItemRow(item: item, list: list)
+                       }
+                   }
+                   Section("Done") {
+                       ForEach(list.items.filter { $0.isDone }) { item in
+                           TodoItemRow(item: item, list: list)
+                       }
+                   }
+               }
         .navigationTitle("Details for \(list.title)")
         .toolbar {
         Button ("Add Todo") {
@@ -56,11 +41,79 @@ struct TodoListView: View {
                 modelContext.insert (todo)
                 list.items.append (todo)
             }
+            
         }
         .id(list.id)
     }
+    
 }
+struct TodoItemRow: View {
+    @Environment(\.modelContext) private var modelContext
+    var item: TodoItem
+    var list: TodoList
+    @State private var isEditing: Bool = false
+    @State private var editedTitle: String = ""
+    var body: some View {
+        HStack {
+            Button {
+                toggleTodoItemCompletion()
+            } label: {
+                Image(systemName: item.isDone ? "circle.fill" : "circle")
+            }
+            Text(item.title)
+                .foregroundStyle(.green)
+            Spacer()
+//            Button("Delete") {
+//                deleteTodoItem()
+//            }
+        }
+        .contextMenu(ContextMenu(menuItems: {
+            Button{
+                deleteTodoItem()
+            } label: {
+                HStack{
+                    Image(systemName: "trash")
+                      
+                      
+                    Text("Delete")
+                       
+                        
+                }
+                .foregroundColor(.red)
+            }
+            Button("Edit"){
+                isEditing = true
+            }
+        }))
+        .alert( "Edit Todo", isPresented: $isEditing) {
+        TextField("Todo Title", text: $editedTitle)
+            Button ("Cancel", role: .cancel, action: {})
+            Button ("Update") {
+                updateTodoItemTitle()
+            }
+            
+        }
+        .id(list.id)
+    }
 
+    private func deleteTodoItem() {
+           if let index = list.items.firstIndex(where: { $0.id == item.id }) {
+               modelContext.delete(list.items.remove(at: index))
+           }
+       }
+
+    private func toggleTodoItemCompletion() {
+           if let index = list.items.firstIndex(where: { $0.id == item.id }) {
+               list.items[index].isDone.toggle()
+           }
+       }
+    private func updateTodoItemTitle() {
+            if let index = list.items.firstIndex(where: { $0.id == item.id }) {
+                list.items[index].title = editedTitle
+               
+            }
+        }
+}
 //#Preview {
 //    TodoListView()
 //}
